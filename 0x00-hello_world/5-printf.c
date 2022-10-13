b756857663771da766a00129aed0be5e99f77669
Confidence Robinson <robinsonconfidence00@gmail.com>
2022-10-13 14:03:46 -0700

    corrections 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main-entry point for the program.
+ *
+ * Retunr:0 if no errors, return non zero if errors
+ */
+int main(void)
 {
 	("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main -is the entry point for the program
+ *
+ * Return:zero if no error, Non-zero value if errors.
+ */
+int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
